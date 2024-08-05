829518920a4ac415eee64dbdaf7b84abc9c0a8ce
GavrikGal <gavrik.gal@gmail.com>
2024-08-05 23:52:13 +0300

    Настроен сервер OAuth 2

--- src/main/java/tacos/authorization/AuthServerApplication.java
+++ src/main/java/tacos/authorization/AuthServerApplication.java
@@ -1,7 +1,13 @@
 package tacos.authorization;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import tacos.authorization.users.User;
+import tacos.authorization.users.UserRepository;
 
 
 @SpringBootApplication
@@ -9,6 +15,26 @@ public class AuthServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner dataLoader(
+            UserRepository repo, PasswordEncoder passwordEncoder) {
+        return args -> {
+            repo.save(
+                    new User("habuma",
+                            passwordEncoder.encode("password"),
+                            "ROLE_ADMIN"));
+            repo.save(
+                    new User("tacochef",
+                            passwordEncoder.encode("password"),
+                            "ROLE_ADMIN"));
+            repo.save(
+                    new User("gal",
+                            passwordEncoder.encode("123"),
+                            "ROLE_ADMIN"));
+
+        };
     }
 
 }
---
+++ src/main/java/tacos/authorization/users/UserRepository.java
@@ -0,0 +1,10 @@
+package tacos.authorization.users;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+}
